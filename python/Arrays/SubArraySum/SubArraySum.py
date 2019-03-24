b1f1ab671aacdb94f91d9ec90bbd4453515875ec
Amit Kushwaha <amit_kushwaha@outlook.com>
2019-03-25 00:15:27 +0100

    Add long test case to verify

--- python/Arrays/SubArraySum/SubArraySum.py
+++ python/Arrays/SubArraySum/SubArraySum.py
@@ -19,3 +19,7 @@ def continuous_sub_array_sum(list_of_number, sum):
 if __name__ == '__main__':
     print(*continuous_sub_array_sum([1, 2, 3, 7, 5], 12))
     print(*continuous_sub_array_sum([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 15))
+
+    s = [ int(x) for x in '135 101 170 125 79 159 163 65 106 146 82 28 162 92 196 143 28 37 192 5 103 154 93 183 22 117 119 96 48 127 172 139 70 113 68 100 36 95 104 12 123 134'.split()]
+    print(*continuous_sub_array_sum(s, 468))
+
